d26096100b04bb96662967052723420ee6ccc976
Pongsak <zajengza@gmail.com>
2024-08-11 15:38:53 +0700

    variable and Operators

---
+++ python1.py
@@ -0,0 +1,86 @@
+"""
+#
+# Part: Python Cooment
+#
+"""
+
+# This is a comment
+# v = s/t
+# v = ความเร็ว (m/s)
+# s = ระยะทาง (m)
+# t = เวลา (s)
+
+"""
+v = s/t
+v = ความเร็ว (m/s
+s = (ระยะทาง)
+t = เวลา(s)
+
+print("Hello World!!!)
+
+"""
+#
+# Part: Python Variables
+#
+x = 5
+y = "Hey Brus"      # Integer
+print(x,y)         # String
+
+x = str(3)
+y = int(5)
+z = float(7)
+print(type(x), type(y), type(z))
+
+"""
+#
+# Part: Variables Names
+#
+"""
+myvar = "john"
+myvar = "john"
+_my_var = "john"
+myVar = "john"
+MYVAR = "john"
+MY_VAR = "john"
+my_var2 = "john"
+
+# Camel Case
+myVariableName = "john"
+# Pascal Case
+MyVariableName = "john"
+# snake Case
+my_variable_name = "john"
+
+"""
+#
+# Part: Python Strin
+# 
+"""
+x = "Hey Brus"
+print(x)
+
+y = """1 Hey Brus
+2 Hey Brus
+3 Hey Brus
+"""
+print(y)
+
+x = "Hey Brus"
+print(x[2])
+print(len(x))
+print("Hey" in x)
+print("what'sup" not in x)
+print(x.upper())
+print(x.lower())
+
+print(x.replace("Brus", "Sis"))
+print(x.split(" "))
+
+a = "Apple"
+b = "company"
+print(a + " " +b)
+
+
+price = 20
+word = f"price: {price:.2f}"
+print(word)
